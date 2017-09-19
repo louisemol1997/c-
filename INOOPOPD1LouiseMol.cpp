91b7bdfd2510fa032bb6bc5ee98a27e45469353f
louisemol1997 <31929716+louisemol1997@users.noreply.github.com>
2017-09-20 00:03:12 +0200

    Add files via upload

---
+++ INOOPOPD1LouiseMol.cpp
@@ -0,0 +1,21 @@
+// INOOPOPD1LouiseMol.cpp : Defines the entry point for the console application.
+//
+
+
+#include <iostream>
+#include "INOOP1LouiseMol.h"
+using namespace std;
+int main()
+{
+	/* oefening van Peggy 
+	int dollars, cents; 
+	double price;
+	char dollarSign = 36;
+	cout<< "enter price in dollars and cents" <<endl;
+	cin	>>price;
+	dollars = price;
+	cents = price * 100 - dollars * 100;
+	cout<< "Total dollars:" << dollarSign << dollars << "Cents:" << cents << endl;
+    return 0;*/
+}
+
